bbf8a8bb66ef83dc7111c81724be7044e6290281
Pecze Tamás <tamaspece@gmail.com>
2020-06-27 18:49:12 +0200

    dynamic change of title language added and minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   public mobileView: boolean;
+  constructor(private _titleService: Title, private _translateService: TranslateService) {}
 
   ngOnInit(): void {
     this.mobileView = window?.innerWidth < 700 ? true : false;
+    this._translateService.onLangChange.subscribe(() => {
+      this._titleService.setTitle(this._translateService.instant('TITLE'));
+  });
+    const browserLang = this._translateService?.getBrowserLang();
+    this._translateService.use(browserLang?.match(/hu|sr/) ? browserLang : 'en');
   }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
-import {TranslateService} from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
@@ -13,13 +13,7 @@ export class HeaderComponent implements OnInit {
   selectedItem = window.location.pathname;
   constructor(private _router: Router, public translate: TranslateService) { }
 
-  ngOnInit(): void {
-    this.translate.addLangs(['en', 'hu', 'sr']);
-    this.translate.setDefaultLang('en');
-
-    const browserLang = this.translate?.getBrowserLang();
-    this.translate.use(browserLang?.match(/hu|sr/) ? browserLang : 'en');
-  }
+  ngOnInit(): void {}
   public navigate(path: string) {
     this.selectedItem = path;
   }
